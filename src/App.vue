6aac42fcbb7c227ce16f0f841a098fb03430cc34
MATIAS <matiasemanuel25@gmail.com>
2025-10-26 01:26:38 -0300

    Preparacion de portafolio Vuejs

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+const nombre = 'Matias Altamiranda'
+</script>
+
+<template>
+  <header>
+    <h1>Portafolio - {{ nombre }}</h1>
+    <p>Portafolio Web</p>
+  </header>
+</template>
+
+<style scoped></style>
